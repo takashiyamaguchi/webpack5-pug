92055d20d9dbfb0bf3365ab8eb60454abcd439af
yamaguchitakashi <yamaguchi@bitbeans.com>
2021-08-04 20:25:00 +0900

    webpack5 asset modules

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,18 @@ module.exports = {
       },
       {
         test: /\.(png|jpg)/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]'
+        },
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              esModule: false,
-              name: 'img/[name].[ext]',
-            }
-          }
+          // {
+          //   loader: 'file-loader',
+          //   options: {
+          //     esModule: false,
+          //     name: 'img/[name].[ext]',
+          //   }
+          // }
         ]
       }
     ],
