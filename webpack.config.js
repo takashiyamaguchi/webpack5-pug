5903b20426defaa2c6656f5e701f210143170c3b
yamaguchitakashi <yamaguchi@bitbeans.com>
2021-08-04 20:17:40 +0900

    file loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: 'img/[name].[ext]',
+            }
+          }
+        ]
+      }
     ],
   },
   plugins: [
